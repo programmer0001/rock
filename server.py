9424bcedb30a978bef79c23dd493602c92368fa2
Toxcik3110 <Toxcik3110@bigmir.net>
2019-02-13 22:52:19 +0200

    trying to fix deploy

--- server.py
+++ server.py
@@ -1,11 +1,14 @@
 import http.server as h                                         # иморт модулей
 import socketserver as s
+from os import environ
+PORT = environ.get('PORT', "8080")
+
 
 PORT = 8080                                                     # порт прослушки
-Handler = h.SimpleHTTPRequestHandler                            # обработчик запросов, отдает
+Handler = h.SimpleHTTPServer                            # обработчик запросов, отдает
                                                                 # статичный файл в текущей папке
 
-with s.TCPServer(("", PORT), Handler) as httpd:                 # описывает сервер
+with s.BaseHTTPServer(('', int(PORT)), Handler) as httpd:                 # описывает сервер
     print("serving at port", PORT)                              # отчет нормальной работы
     httpd.serve_forever()                                       # указзание работать постоянно
     h.send_response(200)
