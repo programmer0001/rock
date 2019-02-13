214a3aa34819bc8e5e587e70bbb45c41005086fd
Toxcik3110 <Toxcik3110@bigmir.net>
2019-02-13 23:15:03 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,13 +1,16 @@
-import http.server as h                                         # иморт модулей
+import http.server as h
 import socketserver as s
 from os import environ
+from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
 
+class HttpProcessor(BaseHTTPRequestHandler):
+  def do_GET(self):
+    self.send_response(200)
+    self.send_header('content-type','text/html')
+    self.end_headers()
+    self.wfile.write("hello !")
 
-PORT = environ.get('PORT', "8080")                                          # порт прослушки
-Handler = h.SimpleHTTPRequestHandler                            # обработчик запросов, отдает
-                                                                # статичный файл в текущей папке
+PORT = environ.get('PORT', "8080")
 
-with s.TCPServer(('', int(PORT)), Handler) as httpd:                 # описывает сервер
-    print("serving at port", PORT)                              # отчет нормальной работы
-    httpd.serve_forever()                                       # указзание работать постоянно
-    h.send_response(200)
+serv = HTTPServer(int(PORT),HttpProcessor)
+serv.serve_forever()
