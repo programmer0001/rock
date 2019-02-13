452749af3a50d3be93792c01ad63fc0e6f2216d7
Toxcik3110 <Toxcik3110@bigmir.net>
2019-02-13 22:59:58 +0200

    Update server.py

--- server.py
+++ server.py
@@ -4,10 +4,10 @@ from os import environ
 
 
 PORT = environ.get('PORT', "8080")                                          # порт прослушки
-Handler = h.SimpleHTTPServer                            # обработчик запросов, отдает
+Handler = h.SimpleHTTPRequestHandler                            # обработчик запросов, отдает
                                                                 # статичный файл в текущей папке
 
-with s.BaseHTTPServer(('', int(PORT)), Handler) as httpd:                 # описывает сервер
+with s.TCPServer(('', int(PORT)), Handler) as httpd:                 # описывает сервер
     print("serving at port", PORT)                              # отчет нормальной работы
     httpd.serve_forever()                                       # указзание работать постоянно
     h.send_response(200)
