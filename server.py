47e6b506a6d8dec1d7e952ff7da52ce963217a9d
Toxcik3110 <Toxcik3110@bigmir.net>
2019-02-13 23:33:04 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import http.server as h
 import socketserver as s
 from os import environ
-from http import BaseHTTPRequestHandler,HTTPServer
+from http.server import BaseHTTPRequestHandler,HTTPServer
 
 class HttpProcessor(BaseHTTPRequestHandler):
   def do_GET(self):
@@ -12,5 +12,12 @@ class HttpProcessor(BaseHTTPRequestHandler):
 
 PORT = environ.get('PORT', "8080")
 
-serv = HTTPServer(int(PORT),HttpProcessor)
-serv.serve_forever()
+# serv = HTTPServer(int(PORT),HttpProcessor)
+# serv.serve_forever()
+
+def run(server_class=HTTPServer, handler_class=HttpProcessor):
+    server_address = int(PORT)
+    httpd = server_class(server_address, handler_class)
+    httpd.serve_forever()
+
+run()
