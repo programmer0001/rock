9280009a49fa7c493613d56496ec5ac27b751b72
programmer0001 <39346599+programmer0001@users.noreply.github.com>
2019-02-24 22:41:11 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,26 +1,44 @@
-from http.server import HTTPServer, CGIHTTPRequestHandler
-
-PORT = ("", 8000)
-
-httpd = HTTPServer(PORT, CGIHTTPRequestHandler)
-httpd.serve_forever()
-
-
-
-'''
+from io import BufferedIOBase as iob
 import http.server as h
 import socketserver as s
 from os import environ
 from http.server import BaseHTTPRequestHandler,HTTPServer
+
+ind = '''<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
+        div {
+            font-size: 4em;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            min-height: 90vh;
+            width: 100%;
+            color: forestgreen;
+            font-family: Arial, Helvetica, sans-serif;
+            font-weight: bold;
+        }
+    </style>
+</head>
+<body>
+    <div>Hello, world!</div>
+
+</body>
+</html>'''
 
 class HttpProcessor(BaseHTTPRequestHandler):
   def do_GET(self):
     self.send_response(200)
     self.send_header('content-type','text/html')
     self.end_headers()
-    self.path(index.html)#write("hello !")
+    self.wfile.write(ind.encode())
+    return
 
-PORT = environ.get('PORT', "8080")
+PORT = environ.get('PORT', "8029")
 
 # serv = HTTPServer(int(PORT),HttpProcessor)
 # serv.serve_forever()
@@ -31,4 +49,3 @@ def run(server_class=HTTPServer, handler_class=HttpProcessor):
     httpd.serve_forever()
 
 run()
-'''
