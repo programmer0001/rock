b4db24bb1b132b8595de21687f1b7badc16110fe
programmer0001 <39346599+programmer0001@users.noreply.github.com>
2019-02-26 20:54:42 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,25 +1,25 @@
 from http.server import BaseHTTPRequestHandler as hand
-from http.server import HTTPServer as hs            # import modules
+from http.server import HTTPServer as hs                      # import modules
 from os import environ
 
-PORT = environ.get('PORT', "4002")                  # listening port
+PORT = environ.get('PORT', "4002")                            # listening port
 
-class myHandler(hand):                              # class for handling requests
+class myHandler(hand):                                        # class for handling requests
 	def do_GET(self):
-		self.send_response(200)                     # answer for browser
-		self.send_header('Content-type','text/html')# info about file
+		self.send_response(200)                       # answer for browser
+		self.send_header('Content-type','text/html')  # info about file
 		self.end_headers()
 
-		self.path = "index.html"                    # path to necessary file
-		f = open(self.path)                         # opening and reading file
+		self.path = "index.html"                      # path to necessary file
+		f = open(self.path)                           # opening and reading file
 		ff = f.read()
-		self.wfile.write(ff.encode())               # send file
+		self.wfile.write(ff.encode())                 # send file
 		return
 
 def run(server_class = hs, handler_class = myHandler):
 	server_address = ('', int(PORT))
-	httpd = server_class(server_address, handler_class)# handling request
-	print('Started server on port: ', PORT)        # message about server's work
-	httpd.serve_forever()                          # non-stop working
+	httpd = server_class(server_address, handler_class)   # handling request
+	print('Started server on port: ', PORT)               # message about server's work
+	httpd.serve_forever()                                 # non-stop working
 
 run()
