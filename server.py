6960a88170ff942bcd071c6fe4b4314e5515b011
programmer0001 <39346599+programmer0001@users.noreply.github.com>
2019-02-26 19:47:39 +0200

    Update server.py

--- server.py
+++ server.py
@@ -3,17 +3,45 @@ from http.server import HTTPServer as hs                # import modules
 
 PORT = 4000                                             # listening port
 
+ind = '''
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
+        div {
+            font-size: 4em;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            min-height: 90vh;
+            width: 100%;
+            color: forestgreen;
+            font-family: Arial, Helvetica, sans-serif;
+            font-weight: bold;
+        }
+    </style>
+</head>
+<body> 
+    <div>Hello, world!</div>
+    
+</body>
+</html>
+'''
+
 class myHandler(hand):
-    def do_GET(self):
-	self.path = "index.html"                        # path to necessary file
+   	def do_GET(self):
+		#self.path = "index.html"                        # path to necessary file
 
-	f = open(self.path)                             # opening and reading file
-	ff = f.read()
+		#f = open(self.path)                             # opening and reading file
+		#ff = f.read()
 
-	self.send_response(200)                         # answer for browser
-	self.send_header('Content-type','text/html')    # info about file
-	self.end_headers()
-	self.wfile.write(ff.encode())                   # send file
+		self.send_response(200)                         # answer for browser
+		self.send_header('Content-type','text/html')    # info about file
+		self.end_headers()
+		self.wfile.write(ind.encode())                   # send file
 
 
 def run():
