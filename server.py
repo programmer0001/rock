820493d34f470d80aa96362173912e1878d14484
programmer0001 <39346599+programmer0001@users.noreply.github.com>
2019-02-26 19:11:44 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,24 +1,24 @@
 from http.server import BaseHTTPRequestHandler as hand
-from http.server import HTTPServer as hs                    # import modules
+from http.server import HTTPServer as hs                # import modules
 
-PORT = 4000                                                 # listening port
+PORT = 4000                                             # listening port
 
-class myHandler(hand):                                      # class for handling requests
-	def do_GET(self):
-		self.path = "index.html"                    # path to necessary file
+class myHandler(hand):
+    def do_GET(self):
+	self.path = "index.html"                        # path to necessary file
 
-		f = open(self.path)                         # opening and reading file
-		ff = f.read()
+	f = open(self.path)                             # opening and reading file
+	ff = f.read()
 
-		self.send_response(200)                     # answer for browser
-		self.send_header('Content-type','text/html')# info about file
-		self.end_headers()
-		self.wfile.write(ff.encode())               # send file
+	self.send_response(200)                         # answer for browser
+	self.send_header('Content-type','text/html')    # info about file
+	self.end_headers()
+	self.wfile.write(ff.encode())                   # send file
 
 
 def run():
-	server = hs(('', PORT), myHandler)                  # handling request
-	print('Started server on port: ', PORT)             # message about server's work
-	server.serve_forever()                              # non-stop working
+    server = hs(('', PORT), myHandler)                  # handling request
+    print('Started server on port: ', PORT)             # message about server's work
+    server.serve_forever()                              # non-stop working
 
 run()
